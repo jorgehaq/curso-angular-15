f22e4cb75df94ea176a41576b477fb302b438235
Jorge Alvarez <jorgehaq@gmail.com>
2024-09-23 21:55:35 -0700

    botones e imagenes

---
+++ src/app/botones/botones.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-botones',
+  templateUrl: './botones.component.html',
+  styleUrls: ['./botones.component.css']
+})
+export class BotonesComponent {
+
+    button_disabled:boolean=false
+
+    src:string='';
+}
